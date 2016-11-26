57427b2d5cd18562626b77fd108d6ea5a9e62d29
Dylan Shaw <dylanjshaw@sandiego.edu>
2016-11-26 15:36:38 -0800

    Restriction applied. One vote per user, voter can alter their vote. View reflects aggregate voting results.

--- app/controllers/vote.rb
+++ app/controllers/vote.rb
@@ -12,11 +12,66 @@
 # end
 
 post'/votes' do
-  params.merge(voter_id: session[:user_id])
-  @new_vote = Vote.create(params)
-  if request.xhr?
-    return @new_vote.value.to_s
+  params[:voter_id] = session[:user_id]
+  search_data = {voteable_type: params[:voteable_type], voteable_id: params[:voteable_id], voter_id: session[:user_id]}
+  vote = Vote.find_by(search_data)
+  question = Question.find(params[:voteable_id])
+
+  if vote
+    vote.value = params[:value]
+    vote.save
   else
-    redirect "/#{@new_vote.voteable_type.downcase}s"
+    vote = Vote.create(params)
   end
+
+  if request.xhr?
+   question.count_votes.to_s
+  else
+    redirect "/#{vote.voteable_type.downcase}s"
+  end
+
+
+
+
+  # params[:voter_id] = session[:user_id]
+  # other_vote = Vote.find_by(params)
+
+  # if !other_vote
+  #   new_vote = Vote.create(params)
+  #   params[:value] = params[:value].to_i * -1
+  #   opposing_vote = Vote.find_by(params)
+
+  # end
+
+#   if !other_vote
+#     new_vote = Vote.create(params)
+
+#     params[:value] = (params[:value].to_i * -1).to_s
+
+#     if opposing_vote = Vote.find_by(params)
+#       opposing_vote.destroy
+#     end
+
+#     if request.xhr?
+#       return new_vote.value.to_s
+#     end
+# else
+#   # else
+#   #   if request.xhr?
+#   #     return "0"
+#   #   # new_vote = other_vote
+#   #   # if request.xhr?
+#   #   #   if new_vote
+#   #   #     return new_vote.value.to_s
+#   #   #   else
+#   #   #     return "0"
+#   #   #   end
+#   #   elsif new_vote
+#   #     redirect "/#{new_vote.voteable_type.downcase}s"
+#   #   else
+#   #     redirect "/#{other_vote.voteable_type.downcase}s"
+#   #   end
+  # end
+
+
 end
